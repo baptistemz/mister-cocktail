7f8a6beeb9917547f8dda6330c594d427e2d74ca
baptiste mulliez <baptistem59@hotmail.com>
2015-10-15 12:23:51 +0200

    validations

---
+++ app/models/dose.rb
@@ -0,0 +1,12 @@
+class Dose < ActiveRecord::Base
+  belongs_to :cocktail
+  belongs_to :ingredient
+
+  validates :description, presence: true
+  validates :cocktail, presence: true
+  validates :ingredient, presence: true
+  validates :cocktail, :uniqueness => {:scope => [:cocktail, :ingredient]}
+
+
+
+end
