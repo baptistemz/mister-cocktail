06df8869add208ea6a10eaff8a02d3b028cdae8a
baptiste mulliez <baptistem59@hotmail.com>
2015-10-17 16:02:49 +0200

    image presence validation bettered

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -8,6 +8,6 @@ class Cocktail < ActiveRecord::Base
     styles: { medium: "300x300>", thumb: "100x100>" }
 
   validates_attachment_content_type :picture,
-    presence: true
+    presence: true,
     content_type: /\Aimage\/.*\z/
 end
